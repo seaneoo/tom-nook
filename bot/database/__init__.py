87406a6147703cbb9a3e4f32499cb39a56c47c11
Sean O'Connor <so@seano.dev>
2023-10-23 08:55:18 -0600

    Connect to database server

---
+++ bot/database/__init__.py
@@ -0,0 +1,36 @@
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime, String, Table, create_engine
+from sqlalchemy.orm import Session, registry
+from sqlalchemy.orm.decl_api import DeclarativeMeta
+
+mapper_registry = registry()
+
+
+class Base(metaclass=DeclarativeMeta):
+    __abstract__ = True
+    registry = mapper_registry
+    metadata = mapper_registry.metadata
+
+    __init__ = mapper_registry.constructor
+
+
+class Guild(Base):
+    __table__ = Table(
+        "Guilds",
+        Base.metadata,
+        Column("GuildID", String, primary_key=True),
+        Column("CreatedAt", DateTime, default=datetime.now()),
+    )
+
+    def __init__(self, guild_id):
+        self.guild_id = guild_id
+
+
+engine = create_engine("postgresql+psycopg://username:password@localhost:5432/tomnook")
+
+Base.metadata.create_all(bind=engine)
+
+
+def session():
+    return Session(engine)
