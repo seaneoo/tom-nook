816a0724c4a59fe7511190a6319cb5d039ebbe2b
Sean O'Connor <so@seano.dev>
2023-10-23 11:12:28 -0600

    Create database helpers module

--- bot/client/__init__.py
+++ bot/client/__init__.py
@@ -1,7 +1,7 @@
 from discord.ext.commands import Bot
-from sqlalchemy.dialects.postgresql import insert
 
-from bot.database import Guild, session
+from bot.database import session
+from bot.database.helpers import insert_guilds
 from common.logger import logger
 
 commands = ["ping"]
@@ -22,37 +22,7 @@ class TomNook(Bot):
         """Called when the client is done preparing the data received from Discord."""
         logger.info(f"🚀 Ready! Logged on as {self.user}.")
 
-        # TODO: Move this functionality elsewhere
-        with session() as sess:
-            """Inserts any guilds into the database that were not already added
-            (this is used for if the bot joins a guild while it was offline)."""
-            try:
-                guilds = [
-                    Guild(guild_id=guild.id, channel_id=guild.system_channel.id)
-                    for guild in self.guilds
-                ]
-
-                statement = (
-                    insert(Guild.__table__)
-                    .values(
-                        [
-                            {
-                                key: value
-                                for key, value in guild.__dict__.items()
-                                if key != "_sa_instance_state"
-                            }
-                            for guild in guilds
-                        ]
-                    )
-                    .on_conflict_do_nothing(
-                        index_elements=["guild_id"],
-                    )
-                )
-
-                sess.execute(statement)
-                sess.commit()
-            except Exception as e:
-                logger.err(e)
+        insert_guilds(session(), *self.guilds)
 
 
 client = TomNook()
---
+++ bot/database/helpers.py
@@ -0,0 +1,46 @@
+from typing import TYPE_CHECKING
+
+from sqlalchemy.dialects.postgresql import insert
+
+from common.logger import logger
+
+from .models import Guild
+
+if TYPE_CHECKING:
+    from discord import Guild as DiscordGuild
+    from sqlalchemy.orm import Session
+
+
+def insert_guilds(session: "Session", *guilds: "DiscordGuild"):
+    """Insert n Discord guilds into the database.
+
+    Args:
+        session (Session): The ORM session.
+    """
+    with session as s:
+        try:
+            # Map each Discord guild to a Guild ORM object
+            guild_orms = [
+                Guild(guild_id=guild.id, channel_id=guild.system_channel.id)
+                for guild in guilds
+            ]
+            # Get the column values for each Guild ORM object
+            guild_values = [
+                {
+                    key: value
+                    for key, value in guild.__dict__.items()
+                    if key != "_sa_instance_state"
+                }
+                for guild in guild_orms
+            ]
+            # Create the SQL statement
+            statement = (
+                insert(Guild.__table__)
+                .values(guild_values)
+                .on_conflict_do_nothing(index_elements=["guild_id"])
+            )
+            # Execute the SQL statement and commit the changes
+            s.execute(statement)
+            s.commit()
+        except Exception as e:
+            logger.err(e)
