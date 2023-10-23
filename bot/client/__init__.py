3c4184921e8fe19d0ea8ffca2670533cc390e807
Sean O'Connor <so@seano.dev>
2023-10-23 10:21:50 -0600

    Insert guild data upon bot start up

--- bot/client/__init__.py
+++ bot/client/__init__.py
@@ -1,5 +1,7 @@
 from discord.ext.commands import Bot
+from sqlalchemy.dialects.postgresql import insert
 
+from bot.database import Guild, session
 from common.logger import logger
 
 commands = ["ping"]
@@ -19,6 +21,38 @@ class TomNook(Bot):
     async def on_ready(self):
         """Called when the client is done preparing the data received from Discord."""
         logger.info(f"🚀 Ready! Logged on as {self.user}.")
+
+        # TODO: Move this functionality elsewhere
+        with session() as sess:
+            """Inserts any guilds into the database that were not already added
+            (this is used for if the bot joins a guild while it was offline)."""
+            try:
+                guilds = [
+                    Guild(guild_id=guild.id, channel_id=guild.system_channel.id)
+                    for guild in self.guilds
+                ]
+
+                statement = (
+                    insert(Guild.__table__)
+                    .values(
+                        [
+                            {
+                                key: value
+                                for key, value in guild.__dict__.items()
+                                if key != "_sa_instance_state"
+                            }
+                            for guild in guilds
+                        ]
+                    )
+                    .on_conflict_do_nothing(
+                        index_elements=["guild_id"],
+                    )
+                )
+
+                sess.execute(statement)
+                sess.commit()
+            except Exception as e:
+                logger.err(e)
 
 
 client = TomNook()
